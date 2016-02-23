3bb1a37701708028bfbe1976e88a97dc12e43098
user <user@192.168.10.94>
2016-02-23 10:46:15 +0900

    subquery

--- bank/src/main/resources/com/bank/web/sql/subquery.sql
+++ bank/src/main/resources/com/bank/web/sql/subquery.sql
@@ -7,3 +7,33 @@ select eid 직원번호, ename 직원명, jid 업무번호
 from   emp
 where  (eid, jid) in (select eid, jid
  					  from   job_his);
+ 					  
+select j.eid 사원번호, 
+      (select e.ename from emp e where e.eid = j.eid) 사원명,
+      j.did 부서번호, 
+      (select d.dname from dep d where j.did = d.did) 부서명
+from   job_his j;
+
+update emp e
+set e.sal = (
+	select sal
+	from ( select d.did, avg(e2.sal) as sal
+		from dep d, emp e2 where d.did=90
+		and d.did = e2.did
+		group by d.did) t
+	where e.did = t.did	
+)
+where e.did in (
+	select did from dep d2 where d2.did = 90
+);
+
+-- 인라인뷰 : from 절에 정의된 subquery
+
+select e.eid 사원번호, e.ename 사원명, d.did 부서번호, d.dname 부서명
+from emp e, dep d, (select avg(e2.sal) as avg_sal
+					from dep d2, emp e2
+					where d2.did = 90 and d2.did = e2.did) t
+where e.did = d.did and e.sal > t.avg_sal;					
+
+select * from emp
+where did = 90;
